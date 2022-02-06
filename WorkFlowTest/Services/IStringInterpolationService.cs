6e21b0546604cee41e3b277c0323fc7f27882abd
Mark McArthey <mcarthey@gmail.com>
2022-02-06 14:39:26 -0600

    Added implementation stub for StringInterpolationService

--- WorkFlowTest/Services/IStringInterpolationService.cs
+++ WorkFlowTest/Services/IStringInterpolationService.cs
@@ -3,5 +3,14 @@
     public interface IStringInterpolationService
     {
         string Number1();
+        string Number2();
+        string Number3();
+        string Number4();
+        string Number5();
+        string Number6();
+        string Number7();
+        string Number8();
+        string Number9();
+        string Number10();
     }
 }
--- WorkFlowTest/Services/StringInterpolationService.cs
+++ WorkFlowTest/Services/StringInterpolationService.cs
@@ -9,20 +9,6 @@ namespace WorkFlowTest.Services
 {
     // INSTRUCTIONS
 
-    //From provided date
-
-    //1.                January 22, 2019 (right aligned in a 40 character field)
-    //2.2019.01.22
-    //3.Day 22 of January, 2019
-    //4.Year: 2019, Month: 01, Day: 22
-    //5.            Tuesday(10 spaces total, right aligned)
-    //6.     11:01 PM Tuesday(10 spaces total for each including the day-of-week, both right-aligned)
-    //7.h:11, m:01, s:27
-    //8.2019.01.22.11.01.27
-
-    //If you have PI(3.1415) - use var pi = Math.PI;
-    //1. Output as currency
-    //2. Output as right-aligned(10 spaces), number with 3 decimal places
     public class StringInterpolationService : IStringInterpolationService
     {
         private readonly ISystemDate _date;
@@ -32,10 +18,67 @@ namespace WorkFlowTest.Services
             _date = date;
         }
 
+        //1. January 22, 2019 (right aligned in a 40 character field)
         public string Number1()
         {
             var date = _date.Now.ToString("MMMM dd, yyyy");
             return $"{date,40}";
+        }
+
+        //2.2019.01.22
+        public string Number2()
+        {
+            throw new NotImplementedException();
+        }
+
+        //3.Day 22 of January, 2019
+        public string Number3()
+        {
+            throw new NotImplementedException();
+        }
+
+        //4.Year: 2019, Month: 01, Day: 22
+        public string Number4()
+        {
+            throw new NotImplementedException();
+        }
+
+        //5.            Tuesday(10 spaces total, right aligned)
+        public string Number5()
+        {
+            throw new NotImplementedException();
+        }
+
+        //6.     11:01 PM Tuesday(10 spaces total for each including the day-of-week, both right-aligned)
+        public string Number6()
+        {
+            throw new NotImplementedException();
+        }
+
+        //7.h:11, m:01, s:27
+        public string Number7()
+        {
+            throw new NotImplementedException();
+        }
+
+        //8.2019.01.22.11.01.27
+        public string Number8()
+        {
+            throw new NotImplementedException();
+        }
+
+        //If you have PI(3.1415) - use var pi = Math.PI;
+        //9. Output as currency
+        public string Number9()
+        {
+            throw new NotImplementedException();
+        }
+
+        //If you have PI(3.1415) - use var pi = Math.PI;
+        //10. Output as right-aligned(10 spaces), number with 3 decimal places
+        public string Number10()
+        {
+            throw new NotImplementedException();
         }
     }
 }
